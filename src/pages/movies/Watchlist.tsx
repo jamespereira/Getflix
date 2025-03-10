f1a54f42c45410c9faa309efac28d74231ce89d9
James Pereira <james.pereira90@gmail.com>
2025-03-10 23:16:39 +1100

    update empty watchlist message

--- src/pages/movies/Watchlist.tsx
+++ src/pages/movies/Watchlist.tsx
@@ -1,5 +1,6 @@
 import MovieList from "@/components/movies/MovieList";
 import useWatchlist from "@/hooks/useWatchlist";
+import { Link } from "react-router-dom";
 
 function Watchlist() {
   const [watchlist, updateWatchlist] = useWatchlist();
@@ -7,11 +8,20 @@ function Watchlist() {
   return (
     <section className="flex flex-col w-full max-w-[1280px] p-4 md:p-8 lg:p-12">
       <h1 className="text-4xl uppercase">Watchlist</h1>
-      <MovieList
-        movies={watchlist}
-        updateWatchlist={updateWatchlist}
-        watchlist={watchlist}
-      />
+      {watchlist.length ? (
+        <MovieList
+          movies={watchlist}
+          updateWatchlist={updateWatchlist}
+          watchlist={watchlist}
+        />
+      ) : (
+        <div className="flex w-full items-center gap-4 justify-center p-6">
+          <p>
+            No movies found. Use <Link to="/">Search</Link> to add movies to
+            your watchlist!
+          </p>
+        </div>
+      )}
     </section>
   );
 }

9e87468b060043d89e9777f3ffdd37a4969a6986
James Pereira <james.pereira90@gmail.com>
2025-03-11 12:41:27 +1100

    update the 404 / build process to fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import path from "path";
+import path, { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Getflix/",
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "404.html",
-          dest: "",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

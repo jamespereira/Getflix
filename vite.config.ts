102dc7921d3123b9194cccab905251dcdde7bc81
James Pereira <james.pereira90@gmail.com>
2025-03-11 12:37:09 +1100

    update dist folder with 404 on build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Getflix/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "404.html",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
